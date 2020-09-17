e8d5004860ad3394dfe0bf4580ab5cb76d450c69
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-17 21:47:28 +0300

    Added test and removed invalid tests

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -10,30 +10,6 @@ import static com.codeborne.selenide.Selenide.*;
 public class CallBackTest {
 
     @Test
-    void shouldReturnErrorNoNameNoTel() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
-        form.$("[data-test-id=name] input").setValue("");
-        form.$("[data-test-id=phone] input").setValue("");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[type=button]").click();
-
-        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldReturnErrorNameLatPhone(){
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
-        form.$("[data-test-id=name] input").setValue("Anna Ivanova");
-        form.$("[data-test-id=phone] input").setValue("89001234567");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[type=button]").click();
-
-        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
     void shouldReturnErrorNameLat() {
         open("http://localhost:9999");
         SelenideElement form = $("[id=root]");
@@ -79,6 +55,17 @@ public class CallBackTest {
         form.$("[type=button]").click();
 
        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно  для заполнения"));
+    }
+
+    @Test
+    void shouldReturnErrorNoCheckbox() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Анна Иванова");
+        form.$("[data-test-id=phone] input").setValue("+79001234567");
+        form.$("[type=button]").click();
+
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
 }
